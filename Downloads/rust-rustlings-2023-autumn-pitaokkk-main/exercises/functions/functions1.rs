8592f275591fae2767c1d211d1aa999cbeef5b9d
pitaokkk <96687564+pitaokkk@users.noreply.github.com>
2023-10-18 12:58:51 +0800

    update

---
+++ Downloads/rust-rustlings-2023-autumn-pitaokkk-main/exercises/functions/functions1.rs
@@ -0,0 +1,13 @@
+// functions1.rs
+//
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
+// hint.
+
+
+fn call_me() {
+    println!("Hello, I'm the call_me function!");
+}
+
+fn main() {
+    call_me();
+}
---
+++ Downloads/rust-rustlings-2023-autumn-pitaokkk-main/exercises/intro/intro2.rs
@@ -0,0 +1,14 @@
+// intro2.rs
+//
+// Make the code print a greeting to the world.
+//
+// Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
+// hint.
+
+
+
+fn main() {
+
+const S: &str = "word";
+    println!("Hello {S}!,");
+}
